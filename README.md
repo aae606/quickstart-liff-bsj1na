4e0bba2a9c1ec5999147f4ac74586ada15fa797b
aae606 <aae606@gmail.com>
2023-07-06 15:33:46 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# quickstart-liff-bsj1na
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/quickstart-liff-bsj1na)
---
+++ index.js
@@ -0,0 +1,112 @@
+// Import stylesheets
+import './style.css';
+
+// Body element
+const body = document.getElementById('body');
+
+// Button elements
+const btnSend = document.getElementById('btnSend');
+const btnClose = document.getElementById('btnClose');
+const btnShare = document.getElementById('btnShare');
+const btnLogIn = document.getElementById('btnLogIn');
+const btnLogOut = document.getElementById('btnLogOut');
+const btnScanCode = document.getElementById('btnScanCode');
+const btnOpenWindow = document.getElementById('btnOpenWindow');
+
+// Profile elements
+const email = document.getElementById('email');
+const userId = document.getElementById('userId');
+const pictureUrl = document.getElementById('pictureUrl');
+const displayName = document.getElementById('displayName');
+const statusMessage = document.getElementById('statusMessage');
+
+// QR element
+const code = document.getElementById('code');
+const friendShip = document.getElementById('friendShip');
+
+async function main() {
+  // Initialize LIFF app)
+  await liff.init({ liffId: '1661054514-W3LKpN36' });
+  // Try a LIFF function
+  switch (liff.getOS()) {
+    case 'android':
+      body.style.backgroundColor = '#d1f5d3';
+      break;
+    case 'ios':
+      body.style.backgroundColor = '#eeeeee';
+      break;
+  }
+  // ...
+  if (!liff.isInClient()) {
+    if (liff.isLoggedIn()) {
+      btnLogIn.style.display = 'none';
+      btnLogOut.style.display = 'block';
+      getUserProfile();
+    } else {
+      btnLogIn.style.display = 'block';
+      btnLogOut.style.display = 'none';
+    }
+  } else {
+    btnSend.style.display = 'block';
+    getUserProfile();
+  }
+  if (liff.isInClient() && liff.getOS() === 'android') {
+    btnScanCode.style.display = 'block';
+  }
+}
+
+async function getUserProfile() {
+  const profile = await liff.getProfile();
+  pictureUrl.src = profile.pictureUrl;
+  userId.innerHTML = '<b>userId:</b> ' + profile.userId;
+  statusMessage.innerHTML = '<b>statusMessage:</b> ' + profile.statusMessage;
+  displayName.innerHTML = '<b>displayName:</b> ' + profile.displayName;
+  email.innerHTML = '<b>email:</b> ' + liff.getDecodedIDToken().email;
+}
+
+async function sendMsg() {
+  if (
+    liff.getContext().type !== 'none' &&
+    liff.getContext().type !== 'external'
+  ) {
+    await liff.sendMessages([
+      {
+        type: 'text',
+        text: 'This message was sent by sendMessages()',
+      },
+    ]);
+    // alert('Message sent');
+    liff.closeWindow();
+  }
+}
+
+async function scanCode() {
+  const result = await liff.scanCode();
+  code.innerHTML = '<b>Code: </b>' + result.value;
+}
+
+btnLogIn.onclick = () => {
+  liff.login();
+};
+
+btnLogOut.onclick = () => {
+  liff.logout();
+  window.location.reload();
+};
+
+btnSend.onclick = () => {
+  sendMsg();
+};
+
+btnScanCode.onclick = () => {
+  scanCode();
+};
+
+btnOpenWindow.onclick = () => {
+  liff.openWindow({
+    url: window.location.href,
+    external: true,
+  });
+};
+
+main();
